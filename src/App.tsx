1e1b4072e70c606affe3db8b8aa3f4e029bf9f57
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-08-15 09:56:01 -0300

    feat: main.jsx to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import './App.css'
 import { workshopName } from "./variable";
+
+function soma(x: number, y: number): number{
+  return x + y
+}
 
 function App() {
   return (
     <main>
       <h1>{workshopName}</h1>
+      <p>Soma 2 + 2: {soma(2,2)}</p>
     </main>
   );
 }
